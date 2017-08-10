28c778422f4d2cf4d0160222ab2272ceffd5124e
ctam91 <ctam91@gmail.com>
2017-08-10 01:50:36 -0700

    add cheese description functionality

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static ArrayList<String> cheeses = new ArrayList<>();
+    static HashMap<String, String> cheeses = new HashMap<>();
 
     // Request path: /cheese
     @RequestMapping(value = "")
@@ -31,8 +31,8 @@ public class CheeseController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheese(@RequestParam String cheeseName){
-        cheeses.add(cheeseName);
+    public String processAddCheese(@RequestParam String cheeseName, @RequestParam String cheeseDescription){
+        cheeses.put(cheeseName, cheeseDescription);
 
         // Redirect to /cheese
         return "redirect:";
