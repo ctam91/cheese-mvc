32ccd60c1a30dbdb26c6777c939d4460162325c7
ctam91 <ctam91@gmail.com>
2017-08-28 13:29:30 -0700

    add CheeseData

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,26 +1,22 @@
 package org.launchcode.cheesemvc.controllers;
 
 import org.launchcode.cheesemvc.models.Cheese;
+import org.launchcode.cheesemvc.models.CheeseData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
-
-    static ArrayList<Cheese> cheeses = new ArrayList<>();
-    static boolean error = false;
 
     // Request path: /cheese
     @RequestMapping(value = "")
     public String index(Model model){
 
-        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("cheeses", CheeseData.getAll());
         model.addAttribute("title", "My Cheeses" );
         return "cheese/index";
     }
@@ -28,8 +24,6 @@ public class CheeseController {
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String displayAddForm(Model model){
         model.addAttribute("title", "Add Cheese");
-        model.addAttribute("cheeses", cheeses);
-        model.addAttribute("errorMessage", error);
         return "cheese/add";
     }
 
@@ -38,11 +32,10 @@ public class CheeseController {
 
         if(!cheeseName.equals("") & isAlpha(cheeseName)){
             Cheese newCheese = new Cheese(cheeseName, cheeseDescription);
-            cheeses.add(newCheese);
+            CheeseData.add(newCheese);
             // Redirect to /cheese
             return "redirect:";
         } else{
-            error = true;
             return "redirect:/cheese/add";
         }
 
@@ -51,21 +44,15 @@ public class CheeseController {
     @RequestMapping(value="delete", method = RequestMethod.GET)
     public String displayDeleteForm(Model model){
         model.addAttribute("title", "Delete Cheese");
-        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("cheeses", CheeseData.getAll());
         return "cheese/delete";
     }
 
     @RequestMapping(value = "delete", method = RequestMethod.POST)
-    public String processDeleteCheese(@RequestParam ArrayList<String> cheeseNameDelete){
-        for(String cheeseToDelete : cheeseNameDelete){
-            for(Cheese eachCheese : cheeses){
-                if(eachCheese.getName().equals(cheeseToDelete)){
-                    cheeses.remove(eachCheese);
-                    return "redirect:";
-                }
-            }
+    public String processDeleteCheese(@RequestParam int [] cheeseIds){
+        for (int cheeseID : cheeseIds){
+            CheeseData.remove(cheeseID);
         }
-        // Redirect to /cheese
         return "redirect:";
     }
 
