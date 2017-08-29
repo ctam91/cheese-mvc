5392abd8a14b1cbfebfc992a0d75ce8436749904
ctam91 <ctam91@gmail.com>
2017-08-29 13:41:02 -0700

    add User

--- src/main/java/org/launchcode/cheesemvc/controllers/UserController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/UserController.java
@@ -1,9 +1,40 @@
 package org.launchcode.cheesemvc.controllers;
 
+import org.launchcode.cheesemvc.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
+
+    // Request path: /user
+    @RequestMapping(value = "")
+    public String index(Model model){
+        model.addAttribute("title", "Users" );
+        return "user/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model){
+        model.addAttribute("title", "Sign Up");
+        return "user/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute User user, String verify){
+        model.addAttribute("user", user.getUser());
+        model.addAttribute("email", user.getEmail());
+        model.addAttribute("password", user.getPassword());
+        model.addAttribute("verify", verify);
+        model.addAttribute("title", "Sign Up");
+        if(verify.equals(user.getPassword())){
+            return "redirect:";
+        } else{
+            return "user/add";
+        }
+    }
 }
--- src/main/java/org/launchcode/cheesemvc/models/User.java
+++ src/main/java/org/launchcode/cheesemvc/models/User.java
@@ -1,4 +1,38 @@
 package org.launchcode.cheesemvc.models;
 
 public class User {
+
+    private String user;
+    private String email;
+    private String password;
+
+    // Constructor
+    public User() {
+    }
+
+    // Getter and setters
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
+
